d237ab8f06459ad7144d90bad7abaf7c9ebbdfdc
Shubham Kumar Patel <imshubham0610@gmail.com>
2026-07-07 20:17:48 +0530

    second commit

--- Fronted/app.py
+++ Fronted/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import base64
 import os
+import sys
+from pathlib import Path
+
+# --- Ensure project root is in sys.path ---
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
+
 from Backend.database.table import create_table
 from Backend.auth import (
     signup_user, login_user, update_photo,
